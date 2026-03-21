2ae2a4e43d1472f72d1acba9107ca6d2cec6a621
Sankhesh Jhaveri <sankyscorpio@gmail.com>
2026-03-21 14:00:00 +0000

    fix null pointer crash in global state destructor

--- WebGPUDeviceGlobalState.cpp
+++ WebGPUDeviceGlobalState.cpp
@@ -7,14 +7,32 @@
 namespace anari_webgpu {
 
 WebGPUDeviceGlobalState::WebGPUDeviceGlobalState(ANARIDevice d)
-    : helium::BaseGlobalDeviceState(d) {}
+    : helium::BaseGlobalDeviceState(d)
+{}
 
 WebGPUDeviceGlobalState::~WebGPUDeviceGlobalState()
 {
-  wgpuQueueRelease(wgpuQueue);
-  wgpuDeviceRelease(wgpuDevice);
-  wgpuAdapterRelease(wgpuAdapter);
-  wgpuInstanceRelease(wgpuInstance);
+  // Only release handles we own (internally created).
+  // Externally provided (shared) handles are managed by the caller.
+  if (wgpuQueue && ownsQueue) {
+    wgpuQueueRelease(wgpuQueue);
+  }
+  wgpuQueue = nullptr;
+
+  if (wgpuDevice && ownsDevice) {
+    wgpuDeviceRelease(wgpuDevice);
+  }
+  wgpuDevice = nullptr;
+
+  if (wgpuAdapter && ownsAdapter) {
+    wgpuAdapterRelease(wgpuAdapter);
+  }
+  wgpuAdapter = nullptr;
+
+  if (wgpuInstance && ownsInstance) {
+    wgpuInstanceRelease(wgpuInstance);
+  }
+  wgpuInstance = nullptr;
 }
 
 } // namespace anari_webgpu
